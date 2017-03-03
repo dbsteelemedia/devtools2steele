0a51cdba383bcec3f63c7f5a3edcd5a0097950c0
davidsteele <davidbsteele1@gmail.com>
2017-03-03 14:18:09 -0500

    modified css to change site appearance. added image gallery to about page. created a sticky post & changed appearane properties for all sticky posts.

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -10,8 +10,10 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+//Scripts
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
+//add google fonts!
 function load_fonts(){
 	wp_register_style ('googleFonts', 'http://fonts.googleapis.com/css?family=Quicksand|Raleway:700');
 	wp_enqueue_style('googleFonts');
